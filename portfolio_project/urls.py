3f0f725448f20338525b2dfff9e966fe18b9b05a
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-01-17 13:19:21 +0530

    Add temporary create-admin url for restoring

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -7,8 +7,23 @@ from django.urls import path,include
 # api = NinjaAPI()
 
 # api.add_router("", portfolio_router)
+# urls.py
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+# Temporary view to create a superuser
+def create_superuser(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='portfolio',
+            email='portfolio@gmail.com',
+            password='Chinta@761200'  # Change to a secure password
+        )
+        return HttpResponse("Superuser created successfully!")
+    return HttpResponse("Superuser already exists!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('My_portfolio.urls'))
+    path('',include('My_portfolio.urls')),
+    path('create-admin/', create_superuser),
 ]
