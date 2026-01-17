283ced28d11c944cb17da0fc2b3077354cc4bfa4
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-01-17 13:57:26 +0530

    Database changed

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -82,11 +82,19 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# Database
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -1,22 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth.models import User
-from django.http import HttpResponse
+# from ninja import NinjaAPI
+# from My_portfolio.api import router as portfolio_router
 
-# Temporary view to create a superuser
-def create_superuser(request):
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser(
-            username='portfolio',
-            email='portfolio@gmail.com',
-            password='Chinta@761200'  # Change to a secure password
-        )
-        return HttpResponse("Superuser created successfully!")
-    return HttpResponse("Superuser already exists!")
+# api = NinjaAPI()
+
+# api.add_router("", portfolio_router)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('My_portfolio.urls')),
-    path('create-admin/',create_superuser),
+    path('',include('My_portfolio.urls'))
 ]
