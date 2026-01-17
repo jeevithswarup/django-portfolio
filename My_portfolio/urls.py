7fe954ebb4546d671f532713d390298f0e866cf4
jeevithswarup <jeevithswaruptuta@gmail.com>
2026-01-17 08:54:15 +0530

    Update urls.py

--- My_portfolio/urls.py
+++ My_portfolio/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from.views import *
 urlpatterns = [
     path('',Home,name='Home'),
@@ -10,8 +8,3 @@ urlpatterns = [
     path("project/<int:id>/",Project_details, name="Project"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
