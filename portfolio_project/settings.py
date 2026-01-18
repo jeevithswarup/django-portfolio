08c5206f4dd13b72e385cbc8ae89aa2fddaad0d8
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-01-18 09:25:42 +0530

    Resolving the issue in cloudinary pdf upload

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -142,7 +142,13 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'RESOURCE_TYPE':"auto",
 }
+
+CLOUDINARY_STORAGE.update{
+  "SECURE":True
+ }
+
 
 
 
