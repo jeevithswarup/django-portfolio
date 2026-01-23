541a5ed3f1605806b712c675c5cf0bd09c340a24
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-01-23 13:53:31 +0530

    Final Update

--- My_portfolio/models.py
+++ My_portfolio/models.py
@@ -63,7 +63,13 @@ class Intro(models.Model):
         null=True
         )
         about_me=models.TextField()
-        resume_pdf=models.FileField(upload_to='resume_pdf/',blank=True,null=True)
+    
+        resume_pdf = CloudinaryField(
+    resource_type="raw",
+    folder="resume_pdf",
+    blank=True,
+    null=True
+)
      
         footer_name=models.CharField(max_length=50,blank=True)
         def __str__(self):
