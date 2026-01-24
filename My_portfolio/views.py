c9ced38615baf9d976455753784bf2ff4b31f307
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-01-24 13:38:40 +0530

    Changes in scoial media links

--- My_portfolio/views.py
+++ My_portfolio/views.py
@@ -6,7 +6,7 @@ def Home(request):
      projects = Projects.objects.all()   
      skills =Skills.objects.all()
      intro=Intro.objects.first()
-     urls=SocialMediaLink.objects.first()
+     urls=SocialMediaLink.objects.first() or SocialMediaLink()
      context={
          'projects': projects,
           'skills': skills,
