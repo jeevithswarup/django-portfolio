e0a2ce8f79c751e3f57256bf77705369c1fd96e5
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-01-24 15:58:52 +0530

    Fixing 500 error

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -106,13 +106,33 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 # Database
 
 
-DATABASES = {
-    'default':dj_database_url.parse(os.environ.get('DATABASE_URL'),
-     conn_max_age=600,
-     ssl_require=True,  
-    )
-}
+# DATABASES = {
+#     'default':dj_database_url.parse(os.environ.get('DATABASE_URL'),
+#      conn_max_age=600,
+#      ssl_require=True,  
+#     )
+# }c
 
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+if os.environ.get("DATABASE_URL"):
+    # Use Render / Postgres
+    DATABASES = {
+        'default': dj_database_url.parse(
+            os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    # Local development fallback (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
