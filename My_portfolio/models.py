85595a60a2dfb4a4a6ae39d777af12a19a32f819
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-01-24 14:46:11 +0530

    Fixing 500 error

--- My_portfolio/models.py
+++ My_portfolio/models.py
@@ -1,7 +1,7 @@
 
 from django.db import models
 from .storages import ImageStorage
-from cloudinary.models import CloudinaryField
+
 
 class Intro(models.Model):
         logo_name=models.CharField(max_length=30)
@@ -11,7 +11,7 @@ class Intro(models.Model):
         #profile_img=models.ImageField(upload_to='profile_img/', blank=True, null=True)
         profile_img = models.ImageField(upload_to='profile_img/',storage=ImageStorage(),blank=True,null=True)
         about_me=models.TextField()
-        resume_pdf =models.FileField(upload_to="resume_pdf/",storage=ImageStorage(),blank=True,null=True)
+        resume_pdf =models.ImageField(upload_to="resume_pdf/",storage=ImageStorage(),blank=True,null=True)
         footer_name=models.CharField(max_length=50,blank=True)
         def __str__(self):
                return self.profile_name
@@ -38,7 +38,7 @@ class Projects(models.Model):
 #     image=models.ImageField(upload_to="project_image/",blank=True,null=True)
 #     project_pdf = models.FileField(upload_to="project_pdfs/",blank=True,null=True)
     image = models.ImageField(upload_to="project_image/",storage=ImageStorage(),blank=True,null=True)
-    project_pdf = models.FileField(upload_to="project_pdfs/",storage=ImageStorage(),blank=True,null=True)
+    project_pdf = models.ImageField(upload_to="project_pdfs/",storage=ImageStorage(),blank=True,null=True)
     github_link=models.URLField(blank=True)
     def __str__(self):
         return self.title
@@ -60,12 +60,12 @@ class AcademicQualification(models.Model):
      percentage=models.DecimalField(max_digits=5,decimal_places=2)
      start_year=models.PositiveIntegerField()
      end_year=models.PositiveIntegerField()
-     result_pdf=models.FileField(upload_to='results/',storage=ImageStorage(),blank=True,null=True)
+     result_pdf=models.ImageField(upload_to='results/',storage=ImageStorage(),blank=True,null=True)
      def __str__(self):
           return self.qualification
 
 class Certificates(models.Model):
-     certificate_pdf=models.FileField(upload_to='certificates/',storage=ImageStorage(),blank=True,null=True)    
+     certificate_pdf=models.ImageField(upload_to='certificates/',storage=ImageStorage(),blank=True,null=True)    
      certificate_name=models.CharField(max_length=50)
      def __str__(self):
           return self.certificate_name
@@ -77,7 +77,7 @@ class Semester(models.Model):
         related_name="semesters"
     )
      semester_number=models.PositiveSmallIntegerField()
-     semester_result=models.FileField(upload_to='sem_results/',storage=ImageStorage(),blank=True,null=True)
+     semester_result=models.ImageField(upload_to='sem_results/',storage=ImageStorage(),blank=True,null=True)
      def __str__(self):
          return  f"{self.qualification} - Semester {self.semester_number}"
      
